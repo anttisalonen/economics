6d35bd0007eab8c18744599da9c2085a96c2eb05
Antti Salonen <ajsalonen@gmail.com>
2010-01-05 11:03:48 +0100

    tweaked equilibrium test market

--- src/TestEquilibrium.hs
+++ src/TestEquilibrium.hs
@@ -8,8 +8,9 @@ import qualified Utility as U
 import MarketTypes
 import MarketHelpers
 
-pfRice    = P.CobbDouglas 10 0.25 0.25
-pfWheat   = P.CobbDouglas 10 0.25 0.25
+pfRice    = P.CobbDouglas 10 0.02 0.48
+pfWheat   = P.CobbDouglas 10 0.01 0.49
+pfGrain   = P.CobbDouglas 10 0.79 0.01
 pfPig     = P.CobbDouglas 10 0.4  0.1
 pfCow     = P.CobbDouglas 10 0.2  0.3
 pfSheep   = P.CobbDouglas 10 0.3  0.2
@@ -19,16 +20,17 @@ pfMutton  = P.CobbDouglas 10 0.25 0.25
 pfLeather = P.CobbDouglas 10 0.25 0.25
 pfWool    = P.CobbDouglas 10 0.25 0.25
 pfBread   = P.CobbDouglas 10 0.25 0.25
-pfLabor   = P.Constant 1000
+pfLabor   = P.Constant 100
 pfWater   = P.Constant 100000
-pfMachine = P.CobbDouglas 10 0.1 0.4
-pfIron    = P.CobbDouglas 10 0.1 0.4
-pfIronore = P.Constant 10
+pfMachine = P.CobbDouglas 10 0.4 0.1
+pfIron    = P.CobbDouglas 10 0.4 0.1
+pfIronore = P.Constant 1
 
 productionmap = E.fromSeq 
  [
    ("Rice",     ProductionInfo pfRice    "Labor" "Machinery" 9 0.1)
   ,("Wheat",    ProductionInfo pfWheat   "Labor" "Machinery" 9 0.1)
+  ,("Grain",    ProductionInfo pfGrain   "Machinery" "Wheat" 1 0.1)
   ,("Pig",      ProductionInfo pfPig     "Water" "Wheat"   1 0.1)
   ,("Cow",      ProductionInfo pfCow     "Water" "Wheat"   1 0.1)
   ,("Sheep",    ProductionInfo pfSheep   "Water" "Wheat"   1 0.1)
@@ -37,7 +39,7 @@ productionmap = E.fromSeq
   ,("Mutton",   ProductionInfo pfMutton  "Labor" "Sheep"   1 0.1)
   ,("Leather",  ProductionInfo pfLeather "Labor" "Cow"     1 0.1)
   ,("Wool",     ProductionInfo pfWool    "Labor" "Sheep"   1 0.1)
-  ,("Bread",    ProductionInfo pfBread   "Labor" "Wheat"   1 0.1)
+  ,("Bread",    ProductionInfo pfBread   "Water" "Grain"   1 0.1)
   ,("Labor",    ProductionInfo pfLabor   ""      ""        1 0.1)
   ,("Water",    ProductionInfo pfWater   ""      ""        1 0.1)
   ,("Machinery",ProductionInfo pfMachine "Labor" "Iron"    1 0.1)
@@ -63,5 +65,5 @@ utilitymap = E.fromSeq
  ]
 
 initialEconomy :: Economy
-initialEconomy = mkEconomy 100 productionmap utilitymap "Welfare" E.empty
+initialEconomy = mkEconomy 1000 productionmap utilitymap "Welfare" E.empty
 
